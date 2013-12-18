55253267281aa92940a345889ff3fa27c00c94e3
Jason Pierce <jason@2toad.com>
2013-12-17 19:21:59 -0800

    Incremented version number for RTM: v13.12.17.2

--- Projects/Assembly.cs
+++ Projects/Assembly.cs
@@ -18,15 +18,6 @@ using System.Reflection;
 [assembly: AssemblyTrademark("2Toad")]
 [assembly: AssemblyCulture("")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("13.12.17.0")]
-[assembly: AssemblyFileVersion("13.12.17.0")]
+// Version information for an assembly
+[assembly: AssemblyVersion("13.12.17.2")]
+[assembly: AssemblyFileVersion("13.12.17.2")]
