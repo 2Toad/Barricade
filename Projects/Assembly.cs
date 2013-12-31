e5476f39f5768f9f77bcacc5e53112cfe79bf0fb
Jason Pierce <jason@2toad.com>
2013-12-31 13:30:26 -0800

    Incremented version number for RTM: v13.12.31.0

--- Projects/Assembly.cs
+++ Projects/Assembly.cs
@@ -19,5 +19,5 @@ using System.Reflection;
 [assembly: AssemblyCulture("")]
 
 // Version information for an assembly
-[assembly: AssemblyVersion("13.12.18.0")]
-[assembly: AssemblyFileVersion("13.12.18.0")]
+[assembly: AssemblyVersion("13.12.31.0")]
+[assembly: AssemblyFileVersion("13.12.31.0")]
